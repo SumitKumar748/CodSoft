a6a6adfc7fa1b132edfa9d3d2e19ab81600a7d83
SumitKumar748 <140527967+SumitKumar748@users.noreply.github.com>
2023-09-23 01:33:52 +0530

    StudentGradeCalculator.java

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -2,40 +2,42 @@ import java.util.Scanner;
 public class StudentGradeCalculator{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("\t\t\tWELCOME TO STUDENT GRADE CALCULATOR");
+        System.out.println("\n\n\t\tWELCOME TO STUDENT GRADE CALCULATOR");
         int m,s,e,h,ss;
-        double tm,ap;
+        float tm,ap,avgp;
 
-        System.out.println("ENTER YOUR MARKS YOU GET IN THE FOLLOWING SUBJECT");
-        System.out.print("MATHS :     ");
+        System.out.println("ENTER YOUR MARKS YOU SCORED IN THE FOLLOWING SUBJECT");
+        System.out.print("MATHS :               ");
         m=sc.nextInt();
-        System.out.print("SCIENCE :     ");
+        System.out.print("SCIENCE :             ");
         s=sc.nextInt();
-        System.out.print("SOCIAL STUDIES :     ");
+        System.out.print("SOCIAL STUDIES :      ");
         ss=sc.nextInt();
-        System.out.print("ENGLISH :     ");
+        System.out.print("ENGLISH :             ");
         e=sc.nextInt();
-        System.out.print("HINDI :     ");
+        System.out.print("HINDI :               ");
         h=sc.nextInt();
         tm=m+s+ss+e+h;
         ap=(tm/500)*100;
-        System.out.println("Total marks scored:   "+ tm);
-        System.out.println("Average Percentage:   "+ap);
-        if(ap>=91)
-            System.out.println("Your Grade is :      A1");
-        else if(ap<=90 && ap>=81  )
-            System.out.println("Your Grade is :     A2");
-        else if( ap<=80 && ap>=71 )
-            System.out.println("Your Grade is :     B1");
-        else if( ap<=70 && ap>=61)
-            System.out.println("Your Grade is :     B2");
-        else if( ap<=60 && ap>=51 )
-            System.out.println("Your Grade is :     C1");
-        else if( ap<=50 && ap>=41 )
-            System.out.println("Your Grade is :     C2");
-        else if( ap<=40 && ap>=33 )
-            System.out.println("Your Grade is :     D");
-        else if( ap<33 )
-            System.out.println("Your Grade is :     F");
+        avgp=ap;
+        System.out.println("Total marks scored:         "+ tm);
+        System.out.println("Average Percentage:         "+ ap+"%");
+        if(avgp>=91.0)
+            System.out.println("Your Grade is :             A1");
+        else if(avgp<=90.0 && ap>=81.0  )
+            System.out.println("Your Grade is :             A2");
+        else if( avgp<=80.0 && ap>=71.0 )
+            System.out.println("Your Grade is :             B1");
+        else if( avgp<=70.0 && ap>=61.0)
+            System.out.println("Your Grade is :             B2");
+        else if( avgp<=60.0 && ap>=51.0 )
+            System.out.println("Your Grade is :             C1");
+        else if( avgp<=50.0 && ap>=41.0 )
+            System.out.println("Your Grade is :             C2");
+        else if( avgp<=40.0 && ap>=33.0 )
+            System.out.println("Your Grade is :             D");
+        else if ( avgp<33.0 )
+            System.out.println("Your Grade is :             F");
+
     }
 }
